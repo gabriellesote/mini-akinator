2667b059f0d575e2cc8f19f2e89de58ad04d93a9
gabbu <gabrielle.sote@gmail.com>
2025-09-15 18:26:27 -0300

    iniciando akinator

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Escolha um desses animais: \n"
+      "01- Gato\n"
+      "02- Cachorro\n"
+      "03- Calopsita\n"
+      "04- Papagaio\n"
+      "05- Corvo\n"
+      "06- Leão\n"
+      "07- Lobo\n"
+      "08- Tartaruga Marinha\n"
+      "09- Crocodilo\n"
+      "10- Sapo\n")
+
+
+print("\nAgora irei tentar adivinhar qual animal você pensou!")
+
+
